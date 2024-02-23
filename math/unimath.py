3d4c62e29ddd2b443b681b9fdb9375590b1c45fc
root <libaice147@gmail.com>
2024-02-23 11:15:51 +0000

    update math amountOut calculator

--- math/unimath.py
+++ math/unimath.py
@@ -15,8 +15,66 @@ print(price_to_tick(5500))
 
 # 85176
 
-print(q96)
+# print(q96)
 # 79228162514264337593543950336
 
-print(price_to_sqrtp(5000))
-# 5602277097478614198912276234240
+# print(price_to_sqrtp(5000))
+# Pc 5602277097478614198912276234240
+
+# print(price_to_sqrtp(4545))
+# Pa 5341294542274603406682713227264
+
+# print(price_to_sqrtp(5500))
+# Pb 5875717789736564987741329162240
+
+# calculate L
+# print( price_to_sqrtp(5000) * price_to_sqrtp(5500) / ( price_to_sqrtp(5500) - price_to_sqrtp(5000) ) / q96)
+
+# ----------------
+sqrtp_low = price_to_sqrtp(4545)
+sqrtp_cur = price_to_sqrtp(5000)
+sqrtp_upp = price_to_sqrtp(5500)
+
+def liquidity0(amount, pa, pb):
+    if(pa > pb):
+        pa, pb = pb, pa
+    return (amount * (pa * pb) / q96) / (pb - pa)
+
+def liquidity1(amount, pa, pb):
+    if(pa > pb):
+        pa, pb = pb, pa
+    return amount * q96 / (pb - pa)
+
+
+eth = 10**18
+amount_eth = 1 * eth
+amount_usdc = 5000 * eth
+
+liq0 = liquidity0(amount_eth, sqrtp_cur, sqrtp_upp)
+liq1 = liquidity1(amount_usdc, sqrtp_cur, sqrtp_low)
+# print(liq0)
+# print(liq1)
+liq = min(liq0, liq1)
+print("liq",liq )
+# 1517.8823437515099e+18
+
+# ----------------
+def calc_amount0(liq, pa, pb):
+    if(pa > pb):
+        pa, pb = pb, pa
+    return int(liq* q96 * (pb-pa) / (pa * pb))
+
+def calc_amount1(liq, pa, pb):
+    if(pa > pb):
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q96)
+
+
+amount0 = calc_amount0(liq, sqrtp_upp, sqrtp_cur)
+amount1 = calc_amount1(liq, sqrtp_low, sqrtp_cur)
+
+print(amount0)
+# 998976618347425408
+
+print(amount1)
+# 5000 000000000000000000
