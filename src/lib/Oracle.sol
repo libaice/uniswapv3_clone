8e889c78fc11424c10aa96ae3ccb9d58cd7023a7
libaice <libaice147@gmail.com>
2024-03-07 15:32:51 +0800

    add nft render and Oracle interface

--- src/lib/Oracle.sol
+++ src/lib/Oracle.sol
@@ -19,24 +19,21 @@ library Oracle {
         uint16 cardinalityNext
     ) internal returns (uint16 indexUpdated, uint16 cardinalityUpdated) {}
 
-    function grow( Observation[65535] storage self, uint16 current, uint16 next) internal {}
+    function grow(Observation[65535] storage self, uint16 current, uint16 next) internal {}
 
-    function transform(Observation memory last , uint32 timeStamp, int24 tick) internal pure returns (Observation memory) {}
+    function transform(Observation memory last, uint32 timeStamp, int24 tick)
+        internal
+        pure
+        returns (Observation memory)
+    {}
 
-    function lte(
-        uint32 time,
-        uint32 a,
-        uint32 b
-    ) private pure returns (bool) {
+    function lte(uint32 time, uint32 a, uint32 b) private pure returns (bool) {
         // if there hasn't been overflow, no need to adjust
         if (a <= time && b <= time) return a <= b;
 
-        uint256 aAdjusted = a > time ? a : a + 2**32;
-        uint256 bAdjusted = b > time ? b : b + 2**32;
+        uint256 aAdjusted = a > time ? a : a + 2 ** 32;
+        uint256 bAdjusted = b > time ? b : b + 2 ** 32;
 
         return aAdjusted <= bAdjusted;
     }
-
-
-
 }
