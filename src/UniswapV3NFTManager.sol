cc5e74f8a18ca48bc5469372a7cc8f6c8fb18b90
libaice <libaice147@gmail.com>
2024-03-08 11:36:13 +0800

    update NFT position manager

---
+++ src/UniswapV3NFTManager.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.14;
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
+contract UniswapV3NFTManager is ERC721 {
+    error NotAuthorized();
+    error NotEnoughLiquidity();
+    error PositionNotCleared();
+    error SlippageCheckFailed(uint256 amount0, uint256 amount1);
+    error WrongToken();
+
+    address public immutable factory;
+    constructor(address factoryAddress) ERC721("Uniswap V3 Positions", "UNIV3") {
+        factory = factoryAddress;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+       return "";
+    }
+
+}
